a8384f853841b0a549e8c748fb34118ec857e087
JFLekstutis <56451458+JFLekstutis@users.noreply.github.com>
2019-10-13 13:57:58 -0400

    First implementation
    WIP of Recipe Lord, using Food2Fork API.
    -search for recipes by ingredient
    -save bookmarked recipes(using EF)

---
+++ Recipe Lord/Data/Recipe_LordContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Recipe_Lord;
+
+namespace Recipe_Lord.Models
+{
+    public class Recipe_LordContext : DbContext
+    {
+        public Recipe_LordContext (DbContextOptions<Recipe_LordContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Recipe_Lord.Recipe> Recipe { get; set; }
+    }
+}
---
+++ Recipe Lord/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Lord
+{
+    public class Recipe
+    {
+        [Key]
+        public int Recipe_Id { get; set; }
+        public string Source_Url { get; set; }
+        public string Image_Url { get; set; }
+        public string Title { get; set; }
+    }
+}
